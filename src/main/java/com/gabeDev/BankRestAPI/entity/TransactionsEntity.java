a60d365d2b6eeda57d87fcb49107550a30e8a9cb
gabeDev <gabrielamorym2@gmail.com>
2025-11-20 19:51:51 -0300

    feat: add entities - added AccountHolderEntity - added TransactionsEntity - added WalletEntity

---
+++ src/main/java/com/gabeDev/BankRestAPI/entity/TransactionsEntity.java
@@ -0,0 +1,38 @@
+package com.gabeDev.BankRestAPI.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "transactions")
+public class TransactionsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "sender_id", nullable = false)
+    private Long senderId;
+    @Column(name = "receiver_id", nullable = false)
+    private Long receiverId;
+
+    @Column(name = "amount", nullable = false)
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private WalletEntity senderWallet;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private WalletEntity receiverWallet;
+}
