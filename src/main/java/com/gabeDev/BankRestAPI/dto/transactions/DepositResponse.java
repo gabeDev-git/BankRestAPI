3f5c9bee9d801d8e82d3350f292fcdce9b139c8f
gabeDev <gabrielamorym2@gmail.com>
2025-11-23 21:43:49 -0300

    feat: added DepositResponse -Specific response DTO for deposits only

---
+++ src/main/java/com/gabeDev/BankRestAPI/dto/transactions/DepositResponse.java
@@ -0,0 +1,12 @@
+package com.gabeDev.BankRestAPI.dto.transactions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.gabeDev.BankRestAPI.entity.TransactionsType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record DepositResponse(Long transactionId, Long receiverId, BigDecimal amount,
+                              @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime createdAt,
+                              TransactionsType type) {
+}
