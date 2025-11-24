d58c9350f5f5401cfcf5ce834dd25368bc740e33
gabeDev <gabrielamorym2@gmail.com>
2025-11-23 21:49:52 -0300

    refactor: overhauled TransactionsService -Refactored class code to fit the new project architecture of separating POSTs by Transfer, Deposit or Debit.

--- src/main/java/com/gabeDev/BankRestAPI/service/TransactionsService.java
+++ src/main/java/com/gabeDev/BankRestAPI/service/TransactionsService.java
@@ -1,6 +1,8 @@
 package com.gabeDev.BankRestAPI.service;
 
-import com.gabeDev.BankRestAPI.dto.TransactionsPostRequest;
+import com.gabeDev.BankRestAPI.dto.transactions.DebitRequest;
+import com.gabeDev.BankRestAPI.dto.transactions.DepositRequest;
+import com.gabeDev.BankRestAPI.dto.transactions.TransferRequest;
 import com.gabeDev.BankRestAPI.entity.Transactions;
 import com.gabeDev.BankRestAPI.entity.TransactionsType;
 import com.gabeDev.BankRestAPI.entity.Wallet;
@@ -9,6 +11,8 @@ import com.gabeDev.BankRestAPI.repository.TransactionsRepo;
 import com.gabeDev.BankRestAPI.repository.WalletRepo;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TransactionsService {
@@ -25,31 +29,47 @@ public class TransactionsService {
     }
 
     @Transactional
-    public Transactions deposit(TransactionsPostRequest request){
+    public Transactions deposit(DepositRequest request){
         if(request.type() != TransactionsType.DEPOSIT){
             throw new RuntimeException(("Transaction type is not DEPOSIT"));
         }
+
         Wallet receiverWallet = walletService.deposit(request.amount(), request.receiverId());
-        return TransactionsMapper.toEntity(request, null, receiverWallet);
+
+        return transactionsRepo.save(TransactionsMapper.toDeposit(request, receiverWallet));
     }
 
     @Transactional
-    public Transactions debit(TransactionsPostRequest request){
+    public Transactions debit(DebitRequest request){
         if(request.type() != TransactionsType.DEBIT){
             throw new RuntimeException(("Transaction type is not DEBIT"));
         }
+
         Wallet senderWallet = walletService.debit(request.amount(), request.senderId());
-        return TransactionsMapper.toEntity(request, senderWallet, null);
+        return transactionsRepo.save(TransactionsMapper.toDebit(request, senderWallet));
     }
 
     @Transactional
-    public Transactions transfer(TransactionsPostRequest request){
+    public Transactions transfer(TransferRequest request){
         if(request.type() != TransactionsType.TRANSFER){
             throw new RuntimeException(("Transactions type is not TRANSFER"));
         }
+        if(request.receiverId().equals(request.senderId())){
+            throw new RuntimeException(("Sender and receiver cannot be the same!"));
+        }
+
         Wallet receiverWallet = walletService.deposit(request.amount(), request.receiverId());
         Wallet senderWallet = walletService.debit(request.amount(), request.senderId());
 
-        return TransactionsMapper.toEntity(request, senderWallet, receiverWallet);
+        return transactionsRepo.save(TransactionsMapper.toTransfer(request, senderWallet, receiverWallet));
+    }
+
+    public Transactions findById(Long id){
+        return transactionsRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Entity not found"));
+    }
+
+    public List<Transactions> findAll(){
+        return transactionsRepo.findAll();
     }
 }
