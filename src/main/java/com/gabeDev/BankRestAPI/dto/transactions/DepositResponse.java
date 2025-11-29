e96d02eacc5a1c575dcf6ced8ed194e89d2d5fd3
gabeDev <gabrielamorym2@gmail.com>
2025-11-28 23:48:31 -0300

    refactor: added TransactionsResponse impl to all responses -Added TransactionsResponse implementation to DepositResponse -Added TransactionsResponse implementation to DebitResponse -Added TransactionsResponse implementation to TransferResponse -Added wallet owner names to all responses

--- src/main/java/com/gabeDev/BankRestAPI/dto/transactions/DepositResponse.java
+++ src/main/java/com/gabeDev/BankRestAPI/dto/transactions/DepositResponse.java
@@ -6,7 +6,7 @@ import com.gabeDev.BankRestAPI.entity.TransactionsType;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record DepositResponse(Long transactionId, Long receiverId, BigDecimal amount,
+public record DepositResponse(Long transactionId, Long receiverWalletId,
+                              String receiverName, BigDecimal amount,
                               @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime createdAt,
-                              TransactionsType type) {
-}
+                              TransactionsType type) implements TransactionsResponse {}
