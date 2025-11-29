92d3b961aebc3f623965e2498bab1fefff99ed97
gabeDev <gabrielamorym2@gmail.com>
2025-11-28 23:56:40 -0300

    feat: added AccountHolderServiceTest -Created test class for AccountHolderService -Successfully implemented first test of throwing InvalidAgeException if underage account creation attempt.

---
+++ src/test/java/com/gabeDev/BankRestAPI/service/AccountHolderServiceTest.java
@@ -0,0 +1,37 @@
+package com.gabeDev.BankRestAPI.service;
+
+import com.gabeDev.BankRestAPI.dto.AccountHolderPostRequest;
+
+import com.gabeDev.BankRestAPI.exceptions.InvalidAgeException;
+import com.gabeDev.BankRestAPI.repository.AccountHolderRepo;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class AccountHolderServiceTest {
+
+    @Mock
+    private AccountHolderRepo accountHolderRepo;
+
+    @InjectMocks
+    private AccountHolderService accountHolderService;
+
+    @Test
+    void accountHolderCreationThrowsInvalidAgeExceptionIfUnderage(){
+        AccountHolderPostRequest request = new AccountHolderPostRequest(
+                "Name", "email@email", "lolol",
+                "123123", LocalDate.of(2008, 06, 20)
+        );
+
+        assertThrows(InvalidAgeException.class,
+                () -> accountHolderService.create(request));
+    }
+}
